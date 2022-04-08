c670a733cb9a18a00ccbc1d647bd1bc664e30abd
danimelchor <dmh672@gmail.com>
2022-04-07 20:54:40 -0400

    main commands + db interface

--- Database/Database.java
+++ Database/Database.java
@@ -2,10 +2,29 @@ package Database;
 
 import java.util.HashMap;
 
+import Restaurants.Restaurant;
 import Users.RestaurantOwner;
 import Users.Client;
 
 public class Database {
   private HashMap<String, RestaurantOwner> ownerDatabase;
-  private HashMap<String, Client> userDatabase;
+  private HashMap<String, Client> clientDatabase;
+  private HashMap<String, Restaurant> restaurantDatabase;
+
+  public Restaurant getRestaurantByName(String restaurantName) {return null;}
+
+  public Client getClientByUserName(String userName) {return null;}
+
+  public RestaurantOwner getRestaurantOwnerByUserName(String userName) {return null;}
+
+  public Client authenticateClient(String userName, String password) {return null;}
+
+  public RestaurantOwner authenticateRestaurantOwner(String userName, String password) {return null;}
+
+  public void addClient(Client client) {}
+
+  public void addRestaurantOwner(RestaurantOwner restOwner) {}
+
+  public void addRestaurant(Restaurant rest) {}
+
 }
